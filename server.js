9307cc58da17f22549e16520ce0be95ade5b6cc2
Milan Sony <milansonyofficial@gmail.com>
2025-03-30 16:52:35 +0530

    chore: cors added

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv"
 import bodyParser from "body-parser"
 import routes from "./routes/index.js"
 import ip from "ip"
+import cors from "cors"
 
 // config .env
 dotenv.config()
 
 // creates an express app
 const app = express()
+
+// cors
+app.use(cors());
 
 // body parser
 // parse application/x-www-form-urlencoded
@@ -27,8 +31,8 @@ const PORT = process.env.PORT || 5000
 const localIP = ip.address();
 
 // listen on all network interfaces (0.0.0.0)
-app.listen((process.env.PORT || 5000), '0.0.0.0', () => {
+app.listen((PORT), '0.0.0.0', () => {
     console.log(`\n🚀 Server listening on port: ${PORT}`)
     console.log(`\n🌼 Server running on http://0.0.0.0:${PORT}`)
-    console.log(`\n⭐ Server is accessible at http://${localIP}`)
+    console.log(`\n⭐ Server is accessible at http://${localIP}:${PORT}`)
 })
